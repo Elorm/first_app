fda373115a8a519082ce66ed21f369012bf248fd
illusion <elorm.adjaho@gmail.com>
2013-12-13 00:14:59 +0000

    Initialize Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'a4c6e901e344a450f0eb76970bc194eb5ebb6507ae544e92bfa0c76b5807f36144db0fabe7c2d4556a04a266849c42af465c85662fff8c5e82b57d56017ce417'
